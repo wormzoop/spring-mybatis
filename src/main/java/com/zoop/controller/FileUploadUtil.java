c88bc624b039993adc49dedf04c4cb8867e6e29c
wormzoop <446706489@qq.com>
2017-05-21 10:21:16 +0800

    multipartfile

--- src/main/java/com/zoop/controller/FileUploadUtil.java
+++ src/main/java/com/zoop/controller/FileUploadUtil.java
@@ -1,5 +1,7 @@
 package com.zoop.controller;
 
+import java.io.File;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -13,12 +15,33 @@ import com.zoop.entity.ResponseObject;
 @Controller
 public class FileUploadUtil {
 
+	private static String filePath = "F:/upload/";//文件路径一定要存在不然会文件io异常
+	
+	/**
+	 * 上传多个文件
+	 * @param files 文件
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping(value = "/upload.do")
 	@ResponseBody
-	public ResponseObject upload(@RequestParam(value="file", required=false) MultipartFile file,
+	public ResponseObject upload(@RequestParam(value="files", required=false) MultipartFile[] files,
 			HttpServletRequest request){
 		ResponseObject responseObj = new ResponseObject();
-		
+		if(files == null || files.length == 0){
+			responseObj.setCode("10000");
+			responseObj.setBody(null);
+		}else{
+			for(MultipartFile file : files){
+				try{
+					file.transferTo(new File(filePath + file.getOriginalFilename()));
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+			responseObj.setCode("10000");
+			responseObj.setBody("上传成功");
+		}
 		return responseObj;
 	}
 	
